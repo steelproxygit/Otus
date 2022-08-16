55ab95c68aaf27a98ec5a4b5f556ef1000eb7f30
Andrey Vasilev <andrey.s.vasilev@nw.rt.ru>
2022-08-16 18:59:12 +0300

    new files

--- src/Read_and_Write/reader.py
+++
@@ -1,4 +0,0 @@
-o = open("../Read_and_Write/books.csv", "r")
-r = o.read()
-print(r)
-o.close()
--- src/Read_and_Write/writer.py
+++
@@ -1,4 +0,0 @@
-o = open("../src/Read_and_Write/write.txt", "w")
-text = """text"""
-o.write(text)
-o.close()
